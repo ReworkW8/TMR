e11d09c39b174299a6a2524e58e43c65b768fbde
Rework <reworkw8official@gmail.com>
2026-02-28 15:59:50 +0100

    Add files via upload

---
+++ request.php
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<response>
+  <valid>1</valid>
+  <login>player</login>
+  <nickname>Player</nickname>
+  <nation>World|Europe</nation>
+  <coppers>99999</coppers>
+  <maniacode>TMREWORK</maniacode>
+  <ladderrank>1</ladderrank>
+  <avatar_name></avatar_name>
+  <avatar_url></avatar_url>
+  <avatar_checksum></avatar_checksum>
+  <faultylogin>0</faultylogin>
+  <servers count="0"></servers>
+  <buddies count="0"></buddies>
+  <championships count="0"></championships>
+  <leagues count="0"></leagues>
+  <manialinks>
+    <manialink id="home"><n>Home</n><url></url></manialink>
+  </manialinks>
+  <maniazone>World|Europe</maniazone>
+  <profile>
+    <login>player</login>
+    <nickname>Player</nickname>
+    <nation>World|Europe</nation>
+    <coppers>99999</coppers>
+  </profile>
+  <zones>
+    <zone id="1" name="World" path="World">
+      <zone id="2" name="Europe" path="World|Europe">
+        <zone id="10" name="Germany"        path="World|Europe|Germany"/>
+        <zone id="11" name="France"         path="World|Europe|France"/>
+        <zone id="12" name="United Kingdom" path="World|Europe|United Kingdom"/>
+        <zone id="13" name="Spain"          path="World|Europe|Spain"/>
+        <zone id="14" name="Italy"          path="World|Europe|Italy"/>
+        <zone id="15" name="Netherlands"    path="World|Europe|Netherlands"/>
+        <zone id="16" name="Belgium"        path="World|Europe|Belgium"/>
+        <zone id="17" name="Sweden"         path="World|Europe|Sweden"/>
+        <zone id="18" name="Norway"         path="World|Europe|Norway"/>
+        <zone id="19" name="Denmark"        path="World|Europe|Denmark"/>
+        <zone id="20" name="Finland"        path="World|Europe|Finland"/>
+        <zone id="21" name="Poland"         path="World|Europe|Poland"/>
+        <zone id="22" name="Austria"        path="World|Europe|Austria"/>
+        <zone id="23" name="Switzerland"    path="World|Europe|Switzerland"/>
+        <zone id="24" name="Czech Republic" path="World|Europe|Czech Republic"/>
+        <zone id="25" name="Russia"         path="World|Europe|Russia"/>
+        <zone id="26" name="Portugal"       path="World|Europe|Portugal"/>
+      </zone>
+      <zone id="3" name="America" path="World|America">
+        <zone id="50" name="United States"  path="World|America|United States"/>
+        <zone id="51" name="Canada"         path="World|America|Canada"/>
+        <zone id="52" name="Brazil"         path="World|America|Brazil"/>
+        <zone id="53" name="Mexico"         path="World|America|Mexico"/>
+        <zone id="54" name="Argentina"      path="World|America|Argentina"/>
+      </zone>
+      <zone id="4" name="Asia" path="World|Asia">
+        <zone id="70" name="China"          path="World|Asia|China"/>
+        <zone id="71" name="Japan"          path="World|Asia|Japan"/>
+        <zone id="72" name="South Korea"    path="World|Asia|South Korea"/>
+        <zone id="73" name="India"          path="World|Asia|India"/>
+      </zone>
+      <zone id="5" name="Oceania" path="World|Oceania">
+        <zone id="90" name="Australia"      path="World|Oceania|Australia"/>
+        <zone id="91" name="New Zealand"    path="World|Oceania|New Zealand"/>
+      </zone>
+      <zone id="6" name="Africa" path="World|Africa">
+        <zone id="100" name="South Africa"  path="World|Africa|South Africa"/>
+        <zone id="101" name="Egypt"         path="World|Africa|Egypt"/>
+      </zone>
+    </zone>
+  </zones>
+</response>
